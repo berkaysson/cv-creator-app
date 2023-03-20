6392a2546b504c6ac704916d18ca2f3e301020ac
berkaysson <berkaysonel85@gmail.com>
2023-03-20 20:44:33 +0300

    Add CVData template object and editCVHandler method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import CVForm from './Components/Form/CVForm';
-import CVPreview from './Components/Preview/CVPreview';
-import './Style/App.css';
-import React from 'react';
+import CVForm from "./Components/Form/CVForm";
+import CVPreview from "./Components/Preview/CVPreview";
+import "./Style/App.css";
+import React, { useState } from "react";
+
+const _CVData = {
+  Personal: {
+    FirstName: "",
+    LastName: "",
+    Title: "",
+    Adress: "",
+    Number: "",
+    Email: "",
+    Description: "",
+  },
+  Education: {
+    0: {
+      UniName: "",
+      Degree: "",
+      StartDate: "",
+      EndDate: "",
+    },
+  },
+  Experience: {
+    0: {
+      Position: "",
+      Company: "",
+      StartDate: "",
+      EndDate: "",
+    },
+  },
+};
 
 function App() {
+  const [CVData, setCVData] = useState(_CVData);
 
+  const editCVHandler = (CV) => {
+    setCVData(CV);
+  };
 
   return (
     <div>
-      <CVForm />
-      <CVPreview />
+      <h1>CV Creator</h1>
+      <CVForm CVData={CVData} onEditCV={editCVHandler} />
+      <CVPreview CVData={CVData} />
     </div>
   );
 }
