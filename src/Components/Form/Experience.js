971b8a19daecdfbcc0f6c0969166a2c64e7575f6
berkaysson <berkaysonel85@gmail.com>
2023-03-21 17:09:33 +0300

    Create Experience and ExperienceItem components

--- src/Components/Form/Experience.js
+++ src/Components/Form/Experience.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ExperienceItem from './ExperienceItem';
+import { v4 as uuidv4 } from 'uuid';  // unique id library
 
-const Experience = () => {
+const Experience = ({onChangeExperience}) => {
+  const [experienceItems, setExperienceItems] = useState([]);
+
+  let experienceData = {};
+
+  const changeExperienceItemHandler = (experienceItem, id) => {
+    let indexOfItem = experienceItems.findIndex(obj => obj.id === id);
+    experienceData = {...experienceData, [indexOfItem]: [experienceItem]}
+    onChangeExperience(experienceData);
+    return;
+  };
+
+  const addNewExperienceItem = (ev) => {
+    ev.preventDefault();
+    const newId = uuidv4();
+    const newExperienceItems = [
+      ...experienceItems,
+      {id:newId},
+    ];
+    setExperienceItems(newExperienceItems);
+  };
+
+  const deleteExperienceItem = (id) => {
+    setExperienceItems(experienceItems.filter((item) => item.id !== id));
+  };
+
   return (
     <div>
       <h2>Experience</h2>
-      <ExperienceItem />
-      <button>Add</button>
+      {experienceItems.map((item) => (
+        <ExperienceItem id={item.id} key={item.id} onDeleteExperienceItem={deleteExperienceItem} 
+          onChangeExperienceItem={changeExperienceItemHandler}/>
+      ))}
+      <button onClick={addNewExperienceItem}>Add New Experience</button>
     </div>
   )
 }
--- src/Components/Form/ExperienceItem.js
+++ src/Components/Form/ExperienceItem.js
@@ -1,22 +1,69 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const ExperienceItem = () => {
+const ExperienceItem = (props) => {
+  const [experienceInfo, setExperienceInfo] = useState({
+    position: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setExperienceInfo((prevExperienceInfo) => ({
+      ...prevExperienceInfo,
+      [name]: value,
+    }));
+  };
+
+  const deleteHandler = (ev) => {
+    ev.preventDefault();
+    props.onDeleteExperienceItem(ev.target.id);
+  };
+
+  useEffect(() => {
+    props.onChangeExperienceItem(experienceInfo, props.id);
+  }, [props.onChangeExperienceItem, experienceInfo, props]);
+  
+
   return (
     <div>
       <label>Position</label>
-      <input type="text" placeholder="Position"></input>
+      <input
+        type="text"
+        name="position"
+        value={experienceInfo.position}
+        onChange={handleChange}
+        placeholder="Position"
+      />
 
       <label>Company</label>
-      <input type="text" placeholder="Company"></input>
+      <input
+        type="text"
+        name="company"
+        value={experienceInfo.company}
+        onChange={handleChange}
+        placeholder="Company"
+      />
 
       <label>Start Date</label>
-      <input type="date"></input>
+      <input
+        type="date"
+        name="startDate"
+        value={experienceInfo.startDate}
+        onChange={handleChange}
+      />
       <label>End Date</label>
-      <input type="date"></input>
+      <input
+        type="date"
+        name="endDate"
+        value={experienceInfo.endDate}
+        onChange={handleChange}
+      />
 
-      <button>Delete</button>
+      <button onClick={deleteHandler}>Delete</button>
     </div>
-  )
+  );
 }
 
 export default ExperienceItem;
