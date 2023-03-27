d1c10005726e1e607cfd36e18e42ea564aa54c00
berkaysson <berkaysonel85@gmail.com>
2023-03-27 20:53:43 +0300

    Add all props ad JSX to see if state and CVForm works with CVPreview

--- src/Components/Preview/CVPreview.js
+++ src/Components/Preview/CVPreview.js
@@ -1,7 +1,38 @@
-import React from 'react';
+const CVPreview = ({CVData}) => {
 
-const CVPreview = () => {
-  return <div></div>
-}
+  return (
+    <div>
+    <div>
+      <h2>Personal Information</h2>
+      <div>{CVData.Personal.FirstName} {CVData.Personal.LastName}</div>
+      <div>{CVData.Personal.Title}</div>
+      <div>{CVData.Personal.Address}</div>
+      <div>{CVData.Personal.Number}</div>
+      <div>{CVData.Personal.Email}</div>
+      <div>{CVData.Personal.Description}</div>
+    </div>
+    <div>
+      <h2>Education</h2>
+      {Object.values(CVData.Education).map((educationItem) => (
+        <div key={educationItem.id}>
+          <div>{educationItem.UniName}</div>
+          <div>{educationItem.Degree}</div>
+          <div>{educationItem.StartDate} - {educationItem.EndDate}</div>
+        </div>
+      ))}
+    </div>
+    <div>
+      <h2>Experience</h2>
+      {Object.values(CVData.Experience).map((experienceItem) => (
+        <div key={experienceItem.id}>
+          <div>{experienceItem.Position}</div>
+          <div>{experienceItem.Company}</div>
+          <div>{experienceItem.StartDate} - {experienceItem.EndDate}</div>
+        </div>
+      ))}
+    </div>
+  </div>
+  );
+};
 
 export default CVPreview;
