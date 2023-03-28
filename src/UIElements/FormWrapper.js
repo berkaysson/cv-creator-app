778cb44e9bac64691d6e232670437dd419e51e3e
berkaysson <berkaysonel85@gmail.com>
2023-03-28 20:01:04 +0300

    Create FormWrapper component for Personal, Education and Experience sections

---
+++ src/UIElements/FormWrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const FormWrapper = styled.div`
+  padding: var(--padding);
+  text-align: center;
+  margin-bottom: var(--margin);
+  
+`
+
+export default FormWrapper;
