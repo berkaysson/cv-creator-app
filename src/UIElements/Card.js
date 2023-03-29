29305235a851cd491a879ed8743f630416374bfb
berkaysson <berkaysonel85@gmail.com>
2023-03-29 17:25:53 +0300

    Changed margin of Card

--- src/UIElements/Card.js
+++ src/UIElements/Card.js
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-  margin: 1rem;
+  margin: 0.5rem;
   width: 100%;
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px; 
-  
   `
 
 export default Card;
