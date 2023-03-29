6d48767b0574d732e2c536ccc28c98bb9c496033
berkaysson <berkaysonel85@gmail.com>
2023-03-29 17:25:08 +0300

    Outsource Empty Cv data

---
+++ src/Utilities/EmptyCVData.js
@@ -0,0 +1,33 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const EMPTY_CVData = {
+  Personal: {
+    FirstName: "",
+    LastName: "",
+    Title: "",
+    Adress: "",
+    Number: "",
+    Email: "",
+    Description: "",
+  },
+  Education: {
+    0: {
+      id : uuidv4(),
+      UniName: "",
+      Degree: "",
+      StartDate: "",
+      EndDate: "",
+    },
+  },
+  Experience: {
+    0: {
+      id : uuidv4(),
+      Position: "",
+      Company: "",
+      StartDate: "",
+      EndDate: "",
+    },
+  },
+};
+
+export default EMPTY_CVData;
